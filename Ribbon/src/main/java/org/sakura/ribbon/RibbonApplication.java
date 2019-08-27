ca3ff2f1f9ff99ee2eb72f2fd0fe4273a13cf0a4
Qiyue0726 <1301616594@qq.com>
2019-08-27 22:15:45 +0800

    配置Ribbon模块

--- Ribbon/src/main/java/org/sakura/ribbon/RibbonApplication.java
+++ Ribbon/src/main/java/org/sakura/ribbon/RibbonApplication.java
@@ -1,16 +1,15 @@
-package org.sakura.client2;
+package org.sakura.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
-public class Client2Application {
+public class RibbonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Client2Application.class, args);
+        SpringApplication.run(RibbonApplication.class, args);
     }
 
 }
